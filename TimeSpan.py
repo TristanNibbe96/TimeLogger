18f8423fefbed310f2151a804f2db52589acc210
Tristan Nibbe <tristan.nibbe114@myci.csuci.edu>
2021-05-01 02:13:31 -0700

    moved timespan to its own file

---
+++ TimeSpan.py
@@ -0,0 +1,29 @@
+class TimeSpan:
+    _hour = 0
+    _minute = 0
+
+    def __init__(self, hour, minute):
+        self.set_hour(hour)
+        self.set_minute(minute)
+
+    def __str__(self):
+        string = str(self._hour) + ":" + str(self._minute)
+        return string
+
+    def set_hour(self, hour):
+        if 0 <= hour <= 23:
+            self._hour = hour
+        else:
+            print("Invalid hour entered: " + str(hour))
+
+    def set_minute(self, minute):
+        if 0 <= minute <= 59:
+            self._minute = minute
+        else:
+            print("Invalid minute entered: " + str(minute))
+
+    def get_minute(self):
+        return self._minute
+
+    def get_hour(self):
+        return self._hour
