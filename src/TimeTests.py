71e2340ad1a50e51c59b71898549bcf4a98d0319
Tristan Nibbe <tristan.nibbe114@myci.csuci.edu>
2021-05-02 23:26:06 -0700

    Simpliefied unit test and added another

--- src/TimeTests.py
+++ src/TimeTests.py
@@ -1,26 +1,25 @@
 import unittest
-from src.TimeInterval import TimeInterval
 from src.Time import Time
 from src.Time import Meridian
-from src.Date import Date
-from src.DateTime import DateTime
 
 
 class TestTimeSubtraction(unittest.TestCase):
 
-    def test_minutes_less_than(self):
+    def less_minutes_in_start_than_end(self):
         time_one = Time(12, 40, Meridian.PM)
         time_two = Time(5, 30, Meridian.PM)
 
-        date_one = Date(4, 29, 2021)
-        date_two = Date(4, 29, 2021)
+        difference = time_two - time_one
 
-        date_time_one = DateTime(date_one, time_one)
-        date_time_two = DateTime(date_two, time_two)
+        self.assertEqual(str(difference), "4:10")
 
-        interval = TimeInterval(date_time_one, date_time_two)
+    def more_minutes_in_start_than_end(self):
+        time_one = Time(12, 40, Meridian.PM)
+        time_two = Time(5, 50, Meridian.PM)
 
-        self.assertEqual(str(interval.get_interval()), "4:10")
+        difference = time_two - time_one
+
+        self.assertEqual(str(difference), "5 :10")
 
 
 if __name__ == '__main__':
