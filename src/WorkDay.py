194d6e23ff150cca4bbdd9c374a7c48aebfd27cf
Tristan Nibbe <tristan.nibbe114@myci.csuci.edu>
2021-05-03 20:24:44 -0700

    Added more to workday

--- src/WorkDay.py
+++ src/WorkDay.py
@@ -1,3 +1,4 @@
+from xxlimited import Null
 from TimeInterval import TimeInterval
 from DateTime import DateTime
 
@@ -8,3 +9,15 @@ class WorkDay:
 
     def set_interval_start(self,interval):
         self._current_interval_start = interval
+
+    def get_total_hours_worked(self):
+        self.check_for_incomplete_interval()
+        total = 0
+        #for interval in interval
+        #total += interval
+        return total
+
+
+    def check_for_incomplete_interval(self):
+        if self._current_interval_start != Null:
+            print("Trying to access total hours worked in a day that has an incomplete time interval")
