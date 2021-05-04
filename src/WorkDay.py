7735d0fa4e05d4bda1284f535950947323e82fc0
Tristan Nibbe <tristan.nibbe114@myci.csuci.edu>
2021-05-03 20:33:04 -0700

    Continued work on workday class

--- src/WorkDay.py
+++ src/WorkDay.py
@@ -1,10 +1,10 @@
 from xxlimited import Null
-from TimeInterval import TimeInterval
+from TimeSpan import TimeSpan
 from DateTime import DateTime
 
 
 class WorkDay:
-    _intervals: TimeInterval
+    _intervals = []
     _current_interval_start: DateTime
 
     def set_interval_start(self,interval):
@@ -12,11 +12,13 @@ class WorkDay:
 
     def get_total_hours_worked(self):
         self.check_for_incomplete_interval()
-        total = 0
-        #for interval in interval
-        #total += interval
+        total = TimeSpan(0, 0)
+        for interval in self._intervals:
+            total += interval.get_interval()
         return total
 
+    def append_interval(self,interval):
+        self._intervals.append(interval)
 
     def check_for_incomplete_interval(self):
         if self._current_interval_start != Null:
