0c091f6f86488891999e98e13100fa0832e5c6cb
Tristan Nibbe <tristan.nibbe114@myci.csuci.edu>
2021-05-05 13:59:15 -0700

    Added function to end current interval in Workday class

--- src/WorkDay.py
+++ src/WorkDay.py
@@ -1,13 +1,19 @@
 from xxlimited import Null
 from TimeSpan import TimeSpan
 from DateTime import DateTime
+from TimeInterval import TimeInterval
 
 
 class WorkDay:
     _intervals = []
     _current_interval_start: DateTime
 
-    def set_interval_start(self,interval):
+    def end_current_interval(self, current_interval_end):
+        new_interval = TimeInterval(self._current_interval_start, current_interval_end)
+        self.append_interval(new_interval)
+        self._current_interval_start = Null
+
+    def set_interval_start(self, interval):
         self._current_interval_start = interval
 
     def get_total_hours_worked(self):
@@ -17,7 +23,7 @@ class WorkDay:
             total += interval.get_interval()
         return total
 
-    def append_interval(self,interval):
+    def append_interval(self, interval):
         self._intervals.append(interval)
 
     def check_for_incomplete_interval(self):
